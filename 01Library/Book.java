8276902041609c77e306b39ab44a73bfa3789677
Othman Bichouna <obichouna@stuy.edu>
2016-11-30 05:46:19 -0500

    Finished the book class

--- 01Library/Book.java
+++ 01Library/Book.java
@@ -14,4 +14,32 @@ public class Book{
     this.title = title;
     this.isbn = isbn;
   }
-}
+
+    public String getTitle(){
+	return title;
+    }
+
+    public String getAuthor(){
+	return author;
+    }
+
+    public String getISBN(){
+	return isbn;
+    }
+
+    public void setAuthor(String x){
+	author = x; 
+    }
+    
+    public void setTitle(String x){
+	title = x;
+    }
+    
+    public void setISBN(String x){
+	isbn = x;
+    }
+
+    public String toString(){
+	return "" + title + ";" + author + ";" + isbn;
+    }
+ }
