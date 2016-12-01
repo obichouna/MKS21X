954221da456e89b9c49df6a49a73cab3ff53f4ed
Othman Bichouna <Othman Bichouna>
2016-11-30 22:36:44 -0500

    Worked on LibraryBook

---
+++ 01Library/LibraryBook.java
@@ -0,0 +1,48 @@
+public abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
+    private String callNumber;
+
+    public LibraryBook(String author, String title, String isbn, String callNumber){
+      super(author, title, isbn);
+      this.callNumber = callNumber;
+    }
+
+    public String getTitle(){
+      return title;
+    }
+
+    public String getAuthor(){
+      return author;
+    }
+
+    public String getISBN(){
+      return isbn;
+    }
+
+    public String getCallNumber(){
+      return callNumber;
+    }
+
+    public void setAuthor(String x){
+      author = x;
+    }
+
+    public void setTitle(String x){
+      title = x;
+    }
+
+    public void setISBN(String x){
+      isbn = x;
+    }
+
+    public void setCallNumber(String x){
+      callNumber = x;
+    }
+
+    abstract void checkout(String patron, String due);
+    abstract void returned();
+    abstract String circulationStatus();
+
+    public int compareTo(){
+
+    }
+}
