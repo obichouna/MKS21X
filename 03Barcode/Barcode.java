834894f502772ab4359a59d872eaaabc596555cd
Othman Bichouna <Othman Bichouna>
2016-12-07 23:36:29 -0500

    Worked on it

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -2,14 +2,25 @@ public class Barcode implements Comparable<Barcode>{
 // instance variables
    private String _zip;
    private int _checkDigit;
+   private int test;
 
 // constructors
 //precondtion: _zip.length() = 5 and zip contains only digits.
 //postcondition: throws a runtime exception zip is not the correct length
 //               or zip contains a non digit
 //               _zip and _checkDigit are initialized.
-  public Barcode(String zip) {}
-
+  public Barcode(String zip) {
+    if (zip.length() != 5){
+      throw new RuntimeException();
+    }else{
+      try{
+        test = Integer.parseInt(zip);
+      }
+      catch (NumberFormatException e){
+        throw new RuntimeException();
+      }
+    }
+  }
 // postcondition: Creates a copy of a bar code.
   public Barcode clone(){}
 
