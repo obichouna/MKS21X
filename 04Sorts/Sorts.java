374e5e668165d0bf90e275c0171474e0ffac8a5b
Othman Bichouna <Othman Bichouna>
2016-12-14 00:21:36 -0500

    Got some help from the interwebs to help me garner ideas, possibly not a good idea

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,20 +1,25 @@
 public class Sorts{
-  /**EDIT THIS METHOD TO MATCH YOUR NAME 
+  /**EDIT THIS METHOD TO MATCH YOUR NAME
   */
   public static String name(){
-    return "09.Bichouna.Othman"; 
+    return "09.Bichouna.Othman";
   }
-
-  /**Selection sort of an int array. 
+  /**Selection sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void selectionSort(int[] data){  }
+  public static void selectionSort(int[] data){
     int minimum;
-    for (int index = 0; index < data.length, index++){
-	for (int = x < data.length - index, x++){
-	    if (x 
-	    
+    for (int index = 0; index < data.length - 1, index++){
+      int i = index;
+      for (int = x + index; x < data.length, x++){
+	       if (data[i] > data[x]){
+           i = x; // trying to get position "i" to be the smaller number
+         }
+       }
+      minimum = data[i];
+      data[i] = data[index];
+      data[index] = minimum;
     }
+    return data;
 }
-
